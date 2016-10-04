7f9caba0a096b189e36242aba741f0033e4919f6
Lucien A <lu.aubert84@gmail.com>
2016-10-05 00:59:59 +0200

    Nettoyage

--- public/js/Sketcher.js
+++ public/js/Sketcher.js
@@ -1,52 +1,52 @@
 class Layer {
-    constructor(name, zIndex, width, height, frame) {
-        this.id = Math.round(Math.random()*1000000)%1000000;
-        this.name = name;
-        this.zIndex = zIndex;
-        this.node = document.createElement('canvas');
-        this.node.setAttribute('id', 'sk_layer_'+this.id);
-        frame.appendChild(this.node);
-        this.visible = true;
-        this.width = width;
-        this.height = height;
-        this.node.width = this.width;
-        this.node.height = this.height;
-        this.opacity = 1;
+	constructor(name, zIndex, width, height, frame) {
+		this.id = Math.round(Math.random()*1000000)%1000000;
+		this.name = name;
+		this.zIndex = zIndex;
+		this.node = document.createElement('canvas');
+		this.node.setAttribute('id', 'sk_layer_'+this.id);
+		frame.appendChild(this.node);
+		this.visible = true;
+		this.width = width;
+		this.height = height;
+		this.node.width = this.width;
+		this.node.height = this.height;
+		this.opacity = 1;
 
-        this.update();
-    }
+		this.update();
+	}
 
-    update() {
-        this.node.style.display = this.visible ? 'block' : 'none';
-        this.node.style.zIndex = this.zIndex;
-    }
+	update() {
+		this.node.style.display = this.visible ? 'block' : 'none';
+		this.node.style.zIndex = this.zIndex;
+	}
 
-    toggleVisibility() {
-        this.visible = this.visible ? false : true;
-        this.update();
-    }
+	toggleVisibility() {
+		this.visible = this.visible ? false : true;
+		this.update();
+	}
 
-    setVisibility(visibility) {
-        this.visible = visibility;
-        this.update();
-    }
+	setVisibility(visibility) {
+		this.visible = visibility;
+		this.update();
+	}
 
-    isVisible() {
-        return this.visible;
-    }
+	isVisible() {
+		return this.visible;
+	}
 }
 
 var Color = {
-    white: "#fff",
-    black: "#000",
-    red: "#f00",
-    green: "#0f0",
-    blue: "#00f",
-    orange: "#ffb603",
-    lightblue: "#a6f7ff",
-    lightgreen: "#7abf30",
-    pink: "#f70e93",
-    purple: "#ae22f6"
+	white: "#fff",
+	black: "#000",
+	red: "#f00",
+	green: "#0f0",
+	blue: "#00f",
+	orange: "#ffb603",
+	lightblue: "#a6f7ff",
+	lightgreen: "#7abf30",
+	pink: "#f70e93",
+	purple: "#ae22f6"
 };
 
 
@@ -54,12 +54,12 @@ var Color = {
 var Sketcher = function() {
 	this.frame = document.querySelector("div#sketcher");
 	this.layers = [];
-    this.selectedLayer;
-    this.clicked = false;
-    this.pos = {x:0, y:0};
-    this.width = window.innerWidth;
-    this.height = window.innerHeight;
-    this.color = Color.red;
+	this.selectedLayer;
+	this.clicked = false;
+	this.pos = {x:0, y:0};
+	this.width = window.innerWidth;
+	this.height = window.innerHeight;
+	this.color = Color.red;
 
 	this.frame.style.width = this.width+"px";
 	this.frame.style.height = this.height+"px";
@@ -72,219 +72,219 @@ var Sketcher = function() {
 }
 
 Sketcher.prototype.onMouseUp = function(e) {
-    this.frame.removeEventListener("mousemove", this.onMouseMove);
+	this.frame.removeEventListener("mousemove", this.onMouseMove);
 
-    this.clear(this.getContext(this.layers[0].id));
+	this.clear(this.getContext(this.layers[0].id));
 
-    if(this.clicked) {
-        var ctx = this.getContext(this.selectedLayer);
-        ctx.strokeStyle = this.color;
-        ctx.beginPath();
-        ctx.moveTo(this.pos.x, this.pos.y);
-        ctx.lineTo(e.offsetX, e.offsetY);
-        ctx.closePath();
-        ctx.stroke();
-    }
+	if(this.clicked) {
+		var ctx = this.getContext(this.selectedLayer);
+		ctx.strokeStyle = this.color;
+		ctx.beginPath();
+		ctx.moveTo(this.pos.x, this.pos.y);
+		ctx.lineTo(e.offsetX, e.offsetY);
+		ctx.closePath();
+		ctx.stroke();
+	}
 
-    this.clicked = false;
+	this.clicked = false;
 };
 
 Sketcher.prototype.onMouseDown = function(e) {
-    if(e.buttons == 1 && e.button == 0 && !this.clicked) {
-        this.clicked = true;
-        this.pos = {x:e.offsetX, y:e.offsetY};
-        frame.addEventListener("mousemove", this.onMouseMove.bind(this));
-    }
+	if(e.buttons == 1 && e.button == 0 && !this.clicked) {
+		this.clicked = true;
+		this.pos = {x:e.offsetX, y:e.offsetY};
+		frame.addEventListener("mousemove", this.onMouseMove.bind(this));
+	}
 }
 
 Sketcher.prototype.onMouseMove = function(e) {
-    if(e.offsetX < 0 || e.offsetY < 0 ||  e.offsetX > this.width || e.offsetY > this.height || !this.clicked) {
-        this.onMouseUp(null);
-    } else {
-        var ctx = this.getContext(this.layers[0].id);
-        this.clear(ctx);
-        ctx.strokeStyle = this.color;
-        ctx.beginPath();
-        ctx.moveTo(this.pos.x, this.pos.y);
-        ctx.lineTo(e.offsetX, e.offsetY);
-        ctx.closePath();
-        ctx.stroke();
-    }
+	if(e.offsetX < 0 || e.offsetY < 0 ||  e.offsetX > this.width || e.offsetY > this.height || !this.clicked) {
+		this.onMouseUp(null);
+	} else {
+		var ctx = this.getContext(this.layers[0].id);
+		this.clear(ctx);
+		ctx.strokeStyle = this.color;
+		ctx.beginPath();
+		ctx.moveTo(this.pos.x, this.pos.y);
+		ctx.lineTo(e.offsetX, e.offsetY);
+		ctx.closePath();
+		ctx.stroke();
+	}
 }
 
 Sketcher.prototype.clear = function(ctx) {
-    ctx.clearRect(0, 0, this.width, this.height);
+	ctx.clearRect(0, 0, this.width, this.height);
 }
 
 Sketcher.prototype.getLayer = function(id) {
-    var ret = null;
+	var ret = null;
 
-    this.layers.forEach(function(layer) {
-        if(layer.id == id) {
-            ret = layer;
-        }
-    });
+	this.layers.forEach(function(layer) {
+		if(layer.id == id) {
+			ret = layer;
+		}
+	});
 
-    return ret;
+	return ret;
 }
 
 Sketcher.prototype.getLayerOnLevel = function(n) {
-    var ret = null;
+	var ret = null;
 
-    this.layers.forEach(function(layer) {
-        if(layer.zIndex == n) {
-            ret = layer;
-        }
-    });
+	this.layers.forEach(function(layer) {
+		if(layer.zIndex == n) {
+			ret = layer;
+		}
+	});
 
-    return ret;
+	return ret;
 }
 
 Sketcher.prototype.countLayers = function() {
-    var c = 0;
+	var c = 0;
 
-    this.layers.forEach(function() {
-        c++;
-    });
+	this.layers.forEach(function() {
+		c++;
+	});
 
-    return c;
+	return c;
 }
 
 Sketcher.prototype.getLayers = function() {
-    if(this.countLayers() == 1) {
-        return [];
-    }
+	if(this.countLayers() == 1) {
+		return [];
+	}
 
-    var ret = [];
+	var ret = [];
 
-    var lvl = 1;
-    var layer;
-    while(ret.length < this.countLayers()-1) {
-        layer = this.getLayerOnLevel(lvl++);
-        if(layer != null)
-            ret.push(layer);
-    }
+	var lvl = 1;
+	var layer;
+	while(ret.length < this.countLayers()-1) {
+		layer = this.getLayerOnLevel(lvl++);
+		if(layer != null)
+			ret.push(layer);
+	}
 
-    return ret.reverse();
+	return ret.reverse();
 }
 
 Sketcher.prototype.getContext = function(id) {
-    return this.getLayer(id).node.getContext("2d");
+	return this.getLayer(id).node.getContext("2d");
 }
 
 Sketcher.prototype.addLayer = function(name, zIndex = 0) {
-    var i = this.layers.push(
-        new Layer(
-            name,
-            zIndex == 0 ? this.countLayers() : zIndex,
-            this.width,
-            this.height,
-            this.frame
-        )
-    );
-    this.selectedLayer = this.layers[i-1].id;
+	var i = this.layers.push(
+		new Layer(
+			name,
+			zIndex == 0 ? this.countLayers() : zIndex,
+			this.width,
+			this.height,
+			this.frame
+		)
+	);
+	this.selectedLayer = this.layers[i-1].id;
 }
 
 Sketcher.prototype.addLayerPrompt = function() {
-    var name = prompt("Please enter layer name", "Foreground");
-    if(name === null) {
-        return false;
-    } else {
-        name = name.toLowerCase();
-        this.addLayer(name);
+	var name = prompt("Please enter layer name", "Foreground");
+	if(name === null) {
+		return false;
+	} else {
+		name = name.toLowerCase();
+		this.addLayer(name);
 
-        return true;
-    }
+		return true;
+	}
 }
 
 Sketcher.prototype.getSelectedLayer = function() {
-    return this.selectedLayer;
+	return this.selectedLayer;
 }
 
 Sketcher.prototype.selectLayer = function(id) {
-    var layer = this.getLayer(id);
-    if(layer != null) {
-        this.selectedLayer = layer.id;
-        return true;
-    } else {
-        console.error('No layer with id "'+id+'".');
-        return false;
-    }
+	var layer = this.getLayer(id);
+	if(layer != null) {
+		this.selectedLayer = layer.id;
+		return true;
+	} else {
+		console.error('No layer with id "'+id+'".');
+		return false;
+	}
 }
 
 Sketcher.prototype.deleteLayer = function(id) {
-    var layer = this.getLayer(id);
-    if(layer != null) {
-        layer.node.remove();
-        var index = -1;
-        this.layers.forEach(function(layer, i) {
-            if(layer.id == id) {
-                index = i;
-            }
-        });
+	var layer = this.getLayer(id);
+	if(layer != null) {
+		layer.node.remove();
+		var index = -1;
+		this.layers.forEach(function(layer, i) {
+			if(layer.id == id) {
+				index = i;
+			}
+		});
 
-        this.layers.slice(index).forEach(function(layer) {
-            layer.zIndex--;
-            layer.update();
-        });
-        delete this.layers[index];
-        return true;
-    } else {
-        console.error('No layer named "'+name+'".');
-        return false;
-    }
+		this.layers.slice(index).forEach(function(layer) {
+			layer.zIndex--;
+			layer.update();
+		});
+		delete this.layers[index];
+		return true;
+	} else {
+		console.error('No layer named "'+name+'".');
+		return false;
+	}
 }
 
 Sketcher.prototype.setLayerVisibility = function(id, visibility) {
-    this.getLayer(id).setVisibility(visibility);
+	this.getLayer(id).setVisibility(visibility);
 }
 
 Sketcher.prototype.toggleLayerVisibility = function(id) {
-    this.getLayer(id).toggleVisibility();
+	this.getLayer(id).toggleVisibility();
 }
 
 Sketcher.prototype.raiseLayer = function(id) {
-    var layer = this.getLayer(id);
-    var prev = this.getLayerOnLevel(layer.zIndex+1);
+	var layer = this.getLayer(id);
+	var prev = this.getLayerOnLevel(layer.zIndex+1);
 
-    if(prev == null || prev.name == "trackpad") {
-       return false;
-    } else {
-        layer.zIndex++;
-        layer.update();
+	if(prev == null || prev.name == "trackpad") {
+	   return false;
+	} else {
+		layer.zIndex++;
+		layer.update();
 
-        prev.zIndex--;
-        prev.update();
-    }
+		prev.zIndex--;
+		prev.update();
+	}
 }
 
 Sketcher.prototype.demoteLayer = function(id) {
-    var layer = this.getLayer(id);
-    var next = this.getLayerOnLevel(layer.zIndex-1);
+	var layer = this.getLayer(id);
+	var next = this.getLayerOnLevel(layer.zIndex-1);
 
-    if(next == null || next.name == "trackpad") {
-       return false;
-    } else {
-        layer.zIndex--;
-        layer.update();
+	if(next == null || next.name == "trackpad") {
+	   return false;
+	} else {
+		layer.zIndex--;
+		layer.update();
 
-        next.zIndex++;
-        next.update();
-    }
+		next.zIndex++;
+		next.update();
+	}
 }
 
 Sketcher.prototype.selectColor = function(colorName) {
-    if(colorName in Color) {
-        this.color = Color[colorName]
-        return true;
-    } else {
-        console.error(colorName+" is not a color.");
-        return false;
-    }
+	if(colorName in Color) {
+		this.color = Color[colorName]
+		return true;
+	} else {
+		console.error(colorName+" is not a color.");
+		return false;
+	}
 }
 
 Sketcher.prototype.getSelectedColor = function() {
-    return this.color;
+	return this.color;
 }
 
 var S = new Sketcher();
--- public/js/SketcherUI.js
+++ public/js/SketcherUI.js
@@ -1,143 +1,143 @@
 var SketcherUI = (function(document, window){
-    this.frame = document.querySelector('div#sketcher');
+	this.frame = document.querySelector('div#sketcher');
 
-    // Create UI components containers
-    this.buttons = document.createElement('div');
-    this.palette = document.createElement('div');
-    this.layers = document.createElement('div');
-    this.layersButtons = document.createElement('div');
-    this.layersList = document.createElement('ul');
+	// Create UI components containers
+	this.buttons = document.createElement('div');
+	this.palette = document.createElement('div');
+	this.layers = document.createElement('div');
+	this.layersButtons = document.createElement('div');
+	this.layersList = document.createElement('ul');
 
-    this.buttons.addEventListener('mousedown', function(e) { e.preventDefault(); e.stopPropagation(); });
-    this.palette.addEventListener('mousedown', function(e) { e.preventDefault(); e.stopPropagation(); });
-    this.layers.addEventListener('mousedown', function(e) { e.preventDefault(); e.stopPropagation(); });
+	this.buttons.addEventListener('mousedown', function(e) { e.preventDefault(); e.stopPropagation(); });
+	this.palette.addEventListener('mousedown', function(e) { e.preventDefault(); e.stopPropagation(); });
+	this.layers.addEventListener('mousedown', function(e) { e.preventDefault(); e.stopPropagation(); });
 
-    this.buttons.setAttribute('id', 'sk_buttons');
-    this.palette.setAttribute('id', 'sk_palette');
-    this.layers.setAttribute('id', 'sk_layers');
-    this.layersButtons.setAttribute('id', 'sk_layers_buttons');
-    this.layersList.setAttribute('id', 'sk_layers_list');
+	this.buttons.setAttribute('id', 'sk_buttons');
+	this.palette.setAttribute('id', 'sk_palette');
+	this.layers.setAttribute('id', 'sk_layers');
+	this.layersButtons.setAttribute('id', 'sk_layers_buttons');
+	this.layersList.setAttribute('id', 'sk_layers_list');
 
-    document.body.insertBefore(this.buttons, this.frame);
-    document.body.insertBefore(this.palette, this.frame);
+	document.body.insertBefore(this.buttons, this.frame);
+	document.body.insertBefore(this.palette, this.frame);
 
-    this.layers.appendChild(this.layersList);
-    this.layers.appendChild(this.layersButtons);
-    document.body.insertBefore(this.layers, this.frame);
+	this.layers.appendChild(this.layersList);
+	this.layers.appendChild(this.layersButtons);
+	document.body.insertBefore(this.layers, this.frame);
 
-    function updatePalette() {
-        this.palette.innerHTML = '';
-        while (this.palette.firstChild) {
-            this.palette.removeChild(this.palette.firstChild);
-        }
+	function updatePalette() {
+		this.palette.innerHTML = '';
+		while (this.palette.firstChild) {
+			this.palette.removeChild(this.palette.firstChild);
+		}
 
-        for(var color in Color) {
-            this.palette.innerHTML += '<a class="sk_color" style="background-color:'+Color[color]+';" alt="'+color+'" href="#"></a>';
-        }
+		for(var color in Color) {
+			this.palette.innerHTML += '<a class="sk_color" style="background-color:'+Color[color]+';" alt="'+color+'" href="#"></a>';
+		}
 
-        Array.prototype.forEach.call(
-            this.palette.querySelectorAll('.sk_color'),
-            function(color) {
-                color.addEventListener('click', function(e) {
-                    S.selectColor(this.getAttribute('alt'));
-                });
-            }
-        );
-    }
+		Array.prototype.forEach.call(
+			this.palette.querySelectorAll('.sk_color'),
+			function(color) {
+				color.addEventListener('click', function(e) {
+					S.selectColor(this.getAttribute('alt'));
+				});
+			}
+		);
+	}
 
-    function updateLayers() {
-        while (this.layersList.firstChild) {
-            this.layersList.removeChild(this.layersList.firstChild);
-        }
+	function updateLayers() {
+		while (this.layersList.firstChild) {
+			this.layersList.removeChild(this.layersList.firstChild);
+		}
 
-        S.getLayers().forEach(function(layer) {
-            var li = document.createElement('li');
-            li.setAttribute('data-id', layer.id);
-            li.setAttribute('data-name', layer.name);
-            if(layer.id == S.getSelectedLayer())
-                li.setAttribute('class', 'active');
+		S.getLayers().forEach(function(layer) {
+			var li = document.createElement('li');
+			li.setAttribute('data-id', layer.id);
+			li.setAttribute('data-name', layer.name);
+			if(layer.id == S.getSelectedLayer())
+				li.setAttribute('class', 'active');
 
-            // Create a container for layer commands
-            var div = document.createElement('div');
-            div.setAttribute('class', 'sk_layer_commands');
+			// Create a container for layer commands
+			var div = document.createElement('div');
+			div.setAttribute('class', 'sk_layer_commands');
 
-            // Create buttons for layer commands
-            var aSel = document.createElement('a');
-            aSel.setAttribute('data-action', 'selectLayer');
-            aSel.setAttribute('class', 'sk_layer_link');
-            aSel.innerHTML = layer.name[0].toUpperCase()+layer.name.substr(1);
-            li.appendChild(aSel);
+			// Create buttons for layer commands
+			var aSel = document.createElement('a');
+			aSel.setAttribute('data-action', 'selectLayer');
+			aSel.setAttribute('class', 'sk_layer_link');
+			aSel.innerHTML = layer.name[0].toUpperCase()+layer.name.substr(1);
+			li.appendChild(aSel);
 
-            var aVis = document.createElement('a');
-            aVis.setAttribute('data-action', 'toggleLayerVisibility');
-            aVis.setAttribute('class', 'sk_check');
-            aVis.innerHTML = '<i class="fa fa-eye'+(layer.isVisible() ? '' : '-slash')+'"></i>';
-            div.appendChild(aVis);
+			var aVis = document.createElement('a');
+			aVis.setAttribute('data-action', 'toggleLayerVisibility');
+			aVis.setAttribute('class', 'sk_check');
+			aVis.innerHTML = '<i class="fa fa-eye'+(layer.isVisible() ? '' : '-slash')+'"></i>';
+			div.appendChild(aVis);
 
-            var aRai = document.createElement('a');
-            aRai.setAttribute('data-action', 'raiseLayer');
-            aRai.setAttribute('class', 'sk_check');
-            aRai.innerHTML = '<i class="fa fa-arrow-up"></i>';
-            div.appendChild(aRai);
+			var aRai = document.createElement('a');
+			aRai.setAttribute('data-action', 'raiseLayer');
+			aRai.setAttribute('class', 'sk_check');
+			aRai.innerHTML = '<i class="fa fa-arrow-up"></i>';
+			div.appendChild(aRai);
 
-            var aDel = document.createElement('a');
-            aDel.setAttribute('data-action', 'deleteLayer');
-            aDel.setAttribute('class', 'sk_check');
-            aDel.innerHTML = '<i class="fa fa-trash"></i>';
-            div.appendChild(aDel);
+			var aDel = document.createElement('a');
+			aDel.setAttribute('data-action', 'deleteLayer');
+			aDel.setAttribute('class', 'sk_check');
+			aDel.innerHTML = '<i class="fa fa-trash"></i>';
+			div.appendChild(aDel);
 
-            var aDem = document.createElement('a');
-            aDem.setAttribute('data-action', 'demoteLayer');
-            aDem.setAttribute('class', 'sk_check');
-            aDem.innerHTML = '<i class="fa fa-arrow-down"></i>';
-            div.appendChild(aDem);
+			var aDem = document.createElement('a');
+			aDem.setAttribute('data-action', 'demoteLayer');
+			aDem.setAttribute('class', 'sk_check');
+			aDem.innerHTML = '<i class="fa fa-arrow-down"></i>';
+			div.appendChild(aDem);
 
-            li.appendChild(div);
+			li.appendChild(div);
 
-            this.layersList.appendChild(li);
-        });
+			this.layersList.appendChild(li);
+		});
 
-        Array.prototype.forEach.call(
-            this.layersList.querySelectorAll('a[data-action="selectLayer"], a[data-action="toggleLayerVisibility"], a[data-action="deleteLayer"], a[data-action="raiseLayer"], a[data-action="demoteLayer"]'),
-            function(btn){
+		Array.prototype.forEach.call(
+			this.layersList.querySelectorAll('a[data-action="selectLayer"], a[data-action="toggleLayerVisibility"], a[data-action="deleteLayer"], a[data-action="raiseLayer"], a[data-action="demoteLayer"]'),
+			function(btn){
 				if(btn.parentNode.tagName == "LI") {
 					var id = btn.parentNode.getAttribute("data-id");
 				} else {
 					var id = btn.parentNode.parentNode.getAttribute("data-id");
 				}
-                var action = btn.getAttribute("data-action");
-                btn.addEventListener('click', function(e) {
-                    S[action](id);
-                    updateLayers();
-                });
-            }
-        );
+				var action = btn.getAttribute("data-action");
+				btn.addEventListener('click', function(e) {
+					S[action](id);
+					updateLayers();
+				});
+			}
+		);
 
-    }
+	}
 
-    function addButton(name, func, container, icon = '') {
-        if(icon == '') {
-            container.innerHTML += '<a class="sk_button" id="sk_button_'+name+'" href="#">'+name+'</a>';
-        } else {
-            container.innerHTML += '<a class="sk_button" id="sk_button_'+name+'" href="#"><i alt="'+name+'" class="fa fa-'+icon+'"></a>';
-        }
+	function addButton(name, func, container, icon = '') {
+		if(icon == '') {
+			container.innerHTML += '<a class="sk_button" id="sk_button_'+name+'" href="#">'+name+'</a>';
+		} else {
+			container.innerHTML += '<a class="sk_button" id="sk_button_'+name+'" href="#"><i alt="'+name+'" class="fa fa-'+icon+'"></a>';
+		}
 
-        var b = container.querySelector('#sk_button_'+name);
-        b.addEventListener('click', func);
-    }
+		var b = container.querySelector('#sk_button_'+name);
+		b.addEventListener('click', func);
+	}
 
-    function addNormalButton(name, func, icon = '') {
-        addButton(name, func, buttons, icon);
-    }
+	function addNormalButton(name, func, icon = '') {
+		addButton(name, func, buttons, icon);
+	}
 
-    // Add native components to containers
-    addButton('square', function(e){ console.log('select square'); }, this.buttons, 'square');
-    addButton('addLayer', function(e){ S.addLayerPrompt(); updateLayers(); }, this.layersButtons, 'plus');
-    updatePalette();
-    updateLayers();
+	// Add native components to containers
+	addButton('square', function(e){ console.log('select square'); }, this.buttons, 'square');
+	addButton('addLayer', function(e){ S.addLayerPrompt(); updateLayers(); }, this.layersButtons, 'plus');
+	updatePalette();
+	updateLayers();
 
-    return {
-        addButton: addNormalButton,
-        updateLayers: updateLayers
-    };
+	return {
+		addButton: addNormalButton,
+		updateLayers: updateLayers
+	};
 })(document, window);
